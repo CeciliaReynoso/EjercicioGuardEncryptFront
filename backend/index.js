79e761c828e4975d100b4cce362d5e0628ad1be4
ceciliareynoso <cecilia.reynosom@outlook.com>
2025-01-22 06:04:39 -0500

    configuracion de guardy crypt

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const usuariosRoutes = require("./routes/usuariosRoutes");
+const { manejarErroresMiddleware } = require("./middlewares/middlewares");
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/", usuariosRoutes);
+
+// Middleware de manejo de errores
+app.use(manejarErroresMiddleware);
+
+app.listen(3000, () => {
+  console.log("¡Servidor encendido en el puerto 3000!");
+});
